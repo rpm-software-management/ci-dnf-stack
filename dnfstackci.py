c408c8ddaa8a5983061ce6dc515324fac1a0e4ba
Radek Holy <rholy@redhat.com>
2015-06-05 16:09:33 +0200

    Support building RPMs of tito-enabled projects
    Let users test tito-enabled projects.
    
    Resolves: https://github.com/rholy/dnf-stack-ci/issues/8

---
+++ dnfstackci.py
@@ -0,0 +1,327 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 dnf-stack-ci Authors. See the AUTHORS file
+# found in the top-level directory of this distribution and
+# at https://github.com/rholy/dnf-stack-ci/.
+#
+# Licensed under the GNU General Public License; either version 2,
+# or (at your option) any later version. See the LICENSE file found
+# in the top-level directory of this distribution and at
+# https://github.com/rholy/dnf-stack-ci/. No part of dnf-stack-ci,
+# including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE
+# file.
+
+"""This module represents the software part of the project.
+
+When the module is run as a script, the command line interface to the
+program is started. The interface usage is::
+
+    usage: prog [-h] DESTINATION
+
+    Build RPMs of a tito-enabled project from the checkout in
+    the current working directory. The RPMs will be stored in
+    a subdirectory "packages" of the destination directory.
+
+    positional arguments:
+      DESTINATION  the name of a destination directory (the directory
+                   will be overwritten)
+
+    optional arguments:
+      -h, --help   show this help message and exit
+
+    The "tito" and "mock" executables must be available. If an error
+    occurs the exit status is non-zero.
+
+:var NAME: the name of the project
+:type NAME: unicode
+:var LOGGER: the logger used by this project
+:type LOGGER: logging.Logger
+
+"""
+
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import argparse
+import errno
+import logging
+import os
+import re
+import shutil
+import subprocess
+import sys
+import tempfile
+
+import pkg_resources
+
+
+NAME = 'dnf-stack-ci'
+
+LOGGER = logging.getLogger(NAME)
+
+
+def _indent(text):
+    """Indent all the lines of a text.
+
+    :param text: the text to be indented
+    :type text: unicode
+    :returns: the indented text
+    :rtype: unicode
+
+    """
+    return re.sub(r'^', '    ', text, flags=re.MULTILINE)
+
+
+def _decode_path(path):
+    """Decode a filesystem path string.
+
+    :param path: the filesystem path
+    :type path: str
+    :return: the decoded path
+    :rtype: unicode
+
+    """
+    return path.decode(sys.getfilesystemencoding() or sys.getdefaultencoding())
+
+
+def _remkdir(name, notexists_ok=False):
+    """Re-create a directory.
+
+    :param name: a name of the directory
+    :type name: unicode
+    :param notexists_ok: create the directory if the path does not exist
+       instead of raising an error
+    :type notexists_ok: bool
+    :raises exceptions.OSError: if the directory cannot be re-created
+
+    """
+    try:
+        shutil.rmtree(name)
+    except OSError as err:
+        if not notexists_ok or err.errno != errno.ENOENT:
+            raise
+    os.mkdir(name)
+
+
+def _build_rpms(destdn, last_tag=True):
+    """Build RPMs of a tito-enabled project in the current work. dir.
+
+    The "tito" and "mock" executables must be available. The destination
+    directory will be overwritten.
+
+    :param destdn: the name of a destination directory
+    :type destdn: unicode
+    :param last_tag: build from the latest tag instead of the current HEAD
+    :type last_tag: bool
+    :raises exceptions.OSError: if the destination directory cannot be
+       created or overwritten or if some of the executables cannot be
+       executed
+    :raises exceptions.ValueError: if the build fails
+
+    """
+    LOGGER.info('Building RPMs from %s...', os.getcwdu())
+    _remkdir(destdn, notexists_ok=True)
+    with _MockConfig() as mockcfg:
+        # FIXME: https://github.com/dgoodwin/tito/issues/171
+        command = [
+            'tito', 'build', '--rpm',
+            '--output={}'.format(destdn),
+            '--builder=mock',
+            '--arg=mock={}'.format(mockcfg.cfgfn)]
+        if not last_tag:
+            command.insert(3, '--test')
+        # FIXME: https://github.com/dgoodwin/tito/issues/165
+        tito = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        streams, status = tito.communicate(), tito.poll()
+        stdout, stderr = (
+            stream.decode('utf-8', 'replace') for stream in streams)
+        assert not any('\ufffd' in stream for stream in [stdout, stderr])
+        mdestdn = os.path.join(destdn, 'mock')
+        assert not os.path.exists(mdestdn), "let's not move it *into* the dir"
+        # FIXME: https://github.com/dgoodwin/tito/issues/178
+        try:
+            shutil.move(mockcfg.resultdn, mdestdn)
+        except (OSError, IOError) as err:
+            # Mock could fail, a new mock version could have changed the path
+            # or a new tito version could have changed its output structure.
+            if isinstance(err.filename, str):
+                filename = _decode_path(err.filename)
+            else:
+                filename = err.filename
+            LOGGER.warning("Mock's path %s is not accessible.", filename)
+    LOGGER.log(
+        logging.ERROR if status else logging.DEBUG,
+        '"tito" have exited with %s:\n  standard output:\n%s\n  standard '
+        'error:\n%s', status, _indent(stdout), _indent(stderr))
+    if status:
+        raise ValueError('"tito" failed')
+
+
+def _start_commandline():
+    """Start the command line interface to the program.
+
+    The root logger is configured to write DEBUG+ messages into the
+    destination directory if not configured otherwise. A handler that
+    writes INFO+ messages to :data:`sys.stderr` is added to
+    :const:`.LOGGER`.
+
+    The interface usage is::
+
+        usage: prog [-h] DESTINATION
+
+        Build RPMs of a tito-enabled project from the checkout in
+        the current working directory. The RPMs will be stored in
+        a subdirectory "packages" of the destination directory.
+
+        positional arguments:
+          DESTINATION  the name of a destination directory (the
+                       directory will be overwritten)
+
+        optional arguments:
+          -h, --help   show this help message and exit
+
+        The "tito" and "mock" executables must be available. If an error
+        occurs the exit status is non-zero.
+
+    :raises exceptions.SystemExit: with a non-zero exit status if an
+       error occurs
+
+    """
+    pkgsreldn = 'packages'
+    argparser = argparse.ArgumentParser(
+        description='Build RPMs of a tito-enabled project from the checkout in'
+                    ' the current working directory. The RPMs will be stored '
+                    'in a subdirectory "{}" of the destination directory.'
+                    ''.format(pkgsreldn),
+        epilog='The "tito" and "mock" executables must be available. If an '
+               'error occurs the exit status is non-zero.')
+    argparser.add_argument(
+        'destdn', type=unicode, metavar='DESTINATION',
+        help='the name of a destination directory (the directory will be '
+             'overwritten)')
+    options = argparser.parse_args()
+    try:
+        _remkdir(options.destdn, notexists_ok=True)
+    except OSError:
+        sys.exit('The destination directory cannot be created or overwritten.')
+    try:
+        logging.basicConfig(
+            filename=os.path.join(options.destdn, 'debug.log'),
+            filemode='wt',
+            format='%(asctime)s.%(msecs)03d:%(levelname)s:%(name)s:'
+                   '%(message)s',
+            datefmt='%Y%m%dT%H%M%S',
+            level=logging.DEBUG)
+    except IOError:
+        sys.exit('The destination directory cannot be created or overwritten.')
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter('%(levelname)s %(message)s'))
+    LOGGER.addHandler(handler)
+    try:
+        _build_rpms(os.path.join(options.destdn, pkgsreldn), last_tag=False)
+    except ValueError:
+        sys.exit(
+            'The build have failed. Hopefully the executables have created an '
+            'output in the destination directory.')
+    except OSError:
+        sys.exit(
+            'The destination directory cannot be overwritten or some of the '
+            'executables cannot be executed.')
+
+
+class _MockConfig(object):  # pylint: disable=too-few-public-methods
+
+    """Class representing a mock configuration.
+
+    The instances can be used as a context manager.
+
+    :ivar basedir: the value set as "config_opts['basedir']"
+    :type basedir: unicode | None
+    :ivar root: the value set as "config_opts['root']"
+    :type root: unicode
+    :ivar cfgfn: a name of the file where the configuration is stored
+    :type cfgfn: unicode | None
+
+    """
+
+    def __init__(self):
+        """Initialize the configuration."""
+        self.basedir = None
+        self.root = NAME
+        self.cfgfn = None
+
+    def __enter__(self):
+        """Enter the runtime context related to this object.
+
+        The configuration is written into a file.
+
+        :returns: self
+        :rtype: ._MockConfig
+
+        """
+        self.basedir = _decode_path(tempfile.mkdtemp())
+        template = pkg_resources.resource_string(
+            __name__, b'resources/mock.cfg')
+        template = template.decode('utf-8')
+        file_ = tempfile.NamedTemporaryFile('wb', suffix='.cfg', delete=False)
+        with file_:
+            file_.write(template.format(basedir=self.basedir, root=self.root))
+        self.cfgfn = _decode_path(file_.name)
+        return self
+
+    @property
+    def resultdn(self):
+        """A name of the directory where the results are stored.
+
+        :returns: a name of the directory
+        :rtype: str
+
+        """
+        return os.path.join(self.basedir, self.root, 'result')
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Exit the runtime context related to this object.
+
+        The configuration file and the chroot files are removed.
+
+        The "mock" executable must be available.
+
+        :param exc_type: the type of the exception that caused the
+           context to be exited
+        :type exc_type: type
+        :param exc_value: the instance of the exception
+        :type exc_value: exceptions.BaseException
+        :param traceback: the traceback that encapsulates the call stack
+           at the point where the exception originally occurred
+        :type traceback: types.TracebackType
+        :returns: suppress the exception that caused the context to be
+           exited
+        :rtype: bool
+        :raises exceptions.OSError: if the executable cannot be executed
+
+        """
+        # The rmtree below could fail otherwise because it may miss privileges.
+        # See also: https://bugzilla.redhat.com/show_bug.cgi?id=450726
+        # FIXME: https://bugzilla.redhat.com/show_bug.cgi?id=1221975
+        status = subprocess.call([
+            'mock', '--quiet', '--root={}'.format(self.cfgfn), '--scrub=all'])
+        assert not status, 'mock should be able to remove its files'
+        try:
+            shutil.rmtree(self.basedir)
+        except OSError:
+            assert False, 'temporary directories should be removable'
+        try:
+            os.remove(self.cfgfn)
+        except OSError:
+            assert False, 'temporary files should be removable'
+        self.cfgfn = None
+        return False
+
+
+if __name__ == '__main__':
+    _start_commandline()
---
+++ features/environment.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 dnf-stack-ci Authors. See the AUTHORS file
+# found in the top-level directory of this distribution and
+# at https://github.com/rholy/dnf-stack-ci/.
+#
+# Licensed under the GNU General Public License; either version 2,
+# or (at your option) any later version. See the LICENSE file found
+# in the top-level directory of this distribution and at
+# https://github.com/rholy/dnf-stack-ci/. No part of dnf-stack-ci,
+# including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE
+# file.
+
+"""This module provides the test fixture common to all the features.
+
+Among other things, the fixture contains a tito-enabled project
+directory and an empty working directory which is created before
+every feature.
+
+The :class:`behave.runner.Context` instance passed to the environmental
+controls and to the step implementations is expected to have following
+attributes:
+
+:attr:`!titodn` : :class:`str`
+    A name of the directory with the tito-enabled project.
+:attr:`!workdn` : :class:`str`
+    A name of the working directory.
+
+"""
+
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+import shutil
+import subprocess
+import tempfile
+
+import pkg_resources
+import pygit2
+
+import dnfstackci
+
+
+def before_all(context):
+    """Do the preparation that can be done at the very beginning.
+
+    The "tito" executable must be available.
+
+    :param context: the context as described in the environment file
+    :type context: behave.runner.Context
+    :raises exceptions.IOError: if the tito-enabled project cannot be
+       created
+    :raises exceptions.ValueError: if the tito-enabled project cannot be
+       created
+    :raises exceptions.OSError: if the executable cannot be executed
+    :raises subprocess.CalledProcessError: if the tito-enabled project
+       cannot be created
+
+    """
+    signature = pygit2.Signature(
+        dnfstackci.NAME, '{}@example.com'.format(dnfstackci.NAME))
+    context.titodn = tempfile.mkdtemp()
+    src_spec = pkg_resources.resource_stream(
+        dnfstackci.__name__, 'resources/foo.spec')
+    dst_spec = open(os.path.join(context.titodn, 'foo.spec'), 'wb')
+    with src_spec, dst_spec:
+        shutil.copyfileobj(src_spec, dst_spec)
+    try:
+        repository = pygit2.init_repository(context.titodn)
+        repository.index.add(
+            os.path.relpath(dst_spec.name, repository.workdir))
+        repository.index.write()
+        repository.create_commit(
+            'refs/heads/master', signature, signature, 'Add a spec file.',
+            repository.index.write_tree(), [])
+    # FIXME: https://github.com/libgit2/pygit2/issues/531
+    except Exception:
+        raise ValueError('Git repository creation failed')
+    # FIXME: https://github.com/dgoodwin/tito/issues/171
+    subprocess.check_call(['tito', 'init'], cwd=context.titodn)
+
+
+# FIXME: https://bitbucket.org/logilab/pylint/issue/535
+def before_feature(context, feature):  # pylint: disable=unused-argument
+    """Do the preparation that must be done before every feature.
+
+    :param context: the context as described in the environment file
+    :type context: behave.runner.Context
+    :param feature: the next tested feature
+    :type feature: behave.model.Feature
+
+    """
+    context.workdn = tempfile.mkdtemp()
+
+
+# FIXME: https://bitbucket.org/logilab/pylint/issue/535
+def after_feature(context, feature):  # pylint: disable=unused-argument
+    """Do the preparation that must be done after every feature.
+
+    :param context: the context as described in the environment file
+    :type context: behave.runner.Context
+    :param feature: the next tested feature
+    :type feature: behave.model.Feature
+    :raises exceptions.OSError: if the working directory cannot be
+       removed
+
+    """
+    shutil.rmtree(context.workdn)
+
+
+def after_all(context):
+    """Do the cleanup that can be done at the very end.
+
+    :param context: the context as described in the environment file
+    :type context: behave.runner.Context
+    :raises exceptions.OSError: if the tito-enabled project cannot be
+       removed
+
+    """
+    shutil.rmtree(context.titodn)
