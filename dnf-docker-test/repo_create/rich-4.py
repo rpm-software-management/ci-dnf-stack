61930a7bc0d902f10a8451cd11a7de65c0d9519b
Jaroslav Mracek <jmracek@redhat.com>
2016-03-24 10:17:16 +0100

    Replace rich repos with repo with noarch rpms
    It also add script that builds the repos.
    
    It allows to run tests on multiple arch.

---
+++ dnf-docker-test/repo_create/rich-4.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3 -tt
+
+from rpmfluff import SimpleRpmBuild
+from rpmfluff import YumRepoBuild
+from pathlib import PurePosixPath
+import os
+import shutil
+
+work_file = os.path.realpath(__file__)
+work_dir = os.path.dirname(work_file)
+file_base_mane = PurePosixPath(work_file).stem
+repo_dir = os.path.join(work_dir, file_base_mane)
+temp_dir = os.path.join(repo_dir, 'temp')
+
+if not os.path.exists(repo_dir):
+    os.makedirs(repo_dir)
+
+if not os.path.exists(temp_dir):
+    os.makedirs(temp_dir)
+
+
+os.chdir(temp_dir)
+
+pkgs = []
+rpm = SimpleRpmBuild('TestA', '1.0.0', '1', ['noarch'])
+rpm.add_requires('(TestC if (TestB or TestD))')
+pkgs.append(rpm)
+
+rpm = SimpleRpmBuild('TestB', '1.0.0', '1', ['noarch'])
+pkgs.append(rpm)
+
+rpm = SimpleRpmBuild('TestC', '1.0.0', '1', ['noarch'])
+pkgs.append(rpm)
+
+rpm = SimpleRpmBuild('TestD', '1.0.0', '1', ['noarch'])
+pkgs.append(rpm)
+
+repo = YumRepoBuild(pkgs)
+
+repo.repoDir = repo_dir
+
+repo.make("noarch")
+
+shutil.rmtree(temp_dir)
