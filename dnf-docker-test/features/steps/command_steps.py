235dd0d62f99560e0ba989d932a65f4749170c16
Karel Srot <ksrot@redhat.com>
2017-01-26 12:51:36 +0100

    add step "the command exit code is {values}"

--- dnf-docker-test/features/steps/command_steps.py
+++ dnf-docker-test/features/steps/command_steps.py
@@ -34,6 +34,23 @@ def step_the_command_should_pass(ctx):
 def step_the_command_should_fail(ctx):
     ctx.assertion.assertNotEqual(ctx.cmd_result.returncode, 0)
 
+@then("the command exit code is {values}")
+def step_the_command_exit_code_is(ctx, values):
+    """
+    Compares the exit code of the previous command with a given value or comma separated list of values or ranges, e.g. '1,3,100-102'.
+    """
+    codes = []
+    parts = values.split(',')
+    for part in parts:
+        if '-' in part:  # range a-b
+            (lower, upper) = part.split('-', 1)
+            lower = int(lower.strip())
+            upper = int(upper.strip())
+            codes.extend(range(lower, upper+1))
+        else:
+            codes.append(int(part.strip()))  # just plain number
+    ctx.assertion.assertIn(ctx.cmd_result.returncode, codes)
+
 @then("the command {stream:stdout_stderr} should match exactly")
 def step_the_command_stream_should_match_exactly(ctx, stream):
     ctx.assertion.assertIsNotNone(ctx.text, "Multiline text is not provided")
