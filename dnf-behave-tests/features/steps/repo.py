aa127e40b710e71bbf2564e98138cd66487549e6
Daniel Mach <dmach@redhat.com>
2019-01-04 17:07:47 +0100

    [steps] A step to disable a repo.

--- dnf-behave-tests/features/steps/repo.py
+++ dnf-behave-tests/features/steps/repo.py
@@ -7,4 +7,12 @@ from common import *
 def given_repo_condition(context, repo):
     if "repos" not in context.dnf:
         context.dnf["repos"] = []
-    context.dnf["repos"].append(repo)
+    if repo not in context.dnf["repos"]:
+        context.dnf["repos"].append(repo)
+
+
+@behave.given("I disable the repository \"{repo}\"")
+def given_repo_condition(context, repo):
+    if "repos" not in context.dnf:
+        context.dnf["repos"] = []
+    context.dnf["repos"].remove(repo)
