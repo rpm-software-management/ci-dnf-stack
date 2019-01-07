7d682cd4ba32e8439c854f2059bc7788d1ece705
Daniel Mach <dmach@redhat.com>
2019-01-07 09:40:22 +0100

    [steps] Add step to verify file sha256.

---
+++ dnf-behave-tests/features/steps/common/checksum.py
@@ -0,0 +1,7 @@
+import hashlib
+
+
+def sha256_checksum(data):
+    h = hashlib.new("sha256")
+    h.update(data)
+    return h.hexdigest
--- dnf-behave-tests/features/steps/path.py
+++ dnf-behave-tests/features/steps/path.py
@@ -17,3 +17,13 @@ def the_installroot_path_exists(context, path):
     full_path = os.path.join(context.dnf.installroot, path.lstrip("/"))
     if not os.path.exists(full_path):
         raise AssertionError("Path does not exist: '%s'" % full_path)
+
+
+@behave.then("file sha256 checksums are following")
+def then_Transaction_is_following(context):
+    check_context_table(context, ["Path", "sha256"])
+
+    for path, checksum in context.table:
+        file_checksum = sha256_checksum(open(path, "rb").read())
+        if file_checksum != checksum:
+            raise AssertionError("File sha256 checksum doesn't match (expected: %s, actual: %s): %s" % (checksum, file_checksum, path))
