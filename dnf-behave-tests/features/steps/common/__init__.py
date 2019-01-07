c8823138ff2780f27d0a9f41ad5c86e526b527b1
Daniel Mach <dmach@redhat.com>
2019-01-07 09:40:22 +0100

    [steps] Add step to verify file sha256.

--- dnf-behave-tests/features/steps/common/__init__.py
+++ dnf-behave-tests/features/steps/common/__init__.py
@@ -1,5 +1,6 @@
 from .behave_ext import check_context_table
 from .cmd import run
+from .checksum import sha256_checksum
 from .dnf import parse_transaction_table
 from .rpm import RPM
 from .rpm import diff_rpm_lists
