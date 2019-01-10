ab633d67a540215fb809eb612f9ee30b2b6892f9
Daniel Mach <dmach@redhat.com>
2019-01-10 16:20:54 +0100

    Add scripts for generating test spec files.

---
+++ dnf-behave-tests/fixtures/rpm2spec/repos/download.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+dnf reposync --repoid=fedora --repoid=fedora-debuginfo --repoid=fedora-source -x '*' --destdir . --download-metadata
+
+# metalink doesn't point to the right location
+# edit /etc/yum.repos.d/fedora-updates.repo and use baseurl instead of metalink
+dnf reposync --repoid=updates --repoid=updates-debuginfo --repoid=updates-source -x '*' --destdir . --download-metadata
+
+dnf reposync --repoid=updates-testing --repoid=updates-testing-debuginfo --repoid=updates-testing-source -x '*' --destdir . --download-metadata
