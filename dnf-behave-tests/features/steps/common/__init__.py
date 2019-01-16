39c449c6e28b752519428342da2dda9352d5ac82
Aleš Matěj <amatej@redhat.com>
2019-01-16 16:15:13 +0100

    [steps] Then stdout section contains

--- dnf-behave-tests/features/steps/common/__init__.py
+++ dnf-behave-tests/features/steps/common/__init__.py
@@ -8,3 +8,4 @@ from .rpm import RPM
 from .rpm import diff_rpm_lists
 from .rpmdb import get_rpmdb_rpms
 from .string import splitter
+from .string import extract_section_content_from_text
--- dnf-behave-tests/features/steps/common/string.py
+++ dnf-behave-tests/features/steps/common/string.py
@@ -9,3 +9,24 @@ def splitter(text):
     Split text by ", "
     """
     return SPLITTER_RE.split(text)
+
+
+def extract_section_content_from_text(section_header, text):
+    SECTION_HEADERS = [
+            'Installing:', 'Upgrading:', 'Removing:', 'Downgrading:', 'Installing dependencies:',
+            'Removing unused dependencies:', # dnf install/remove... transaction listing
+            'Installed:', 'Upgraded:', 'Removed:', 'Downgraded:', # dnf install/remove/... result
+            'Installed Packages', 'Available Packages', 'Recently Added Packages' # dnf list
+            ]
+    parsed = ''
+    copy = False
+    for line in text.split('\n'):
+        if (not copy) and section_header == line:
+            copy = True
+            continue
+        if copy:  # copy lines until hitting empty line or another known header
+            if line.strip() and line not in SECTION_HEADERS:
+                parsed += "%s\n" % line
+            else:
+                return parsed
+    return parsed
