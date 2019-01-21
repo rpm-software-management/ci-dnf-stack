739da5fbc74488e664253dc8b24d9f18065a329a
Pavla Kratochvilova <pkratoch@redhat.com>
2019-01-21 08:56:01 +0100

    [steps] Steps to execute commands in dnf shell

---
+++ dnf-behave-tests/features/steps/common/shell.py
@@ -0,0 +1,5 @@
+def stdout_from_shell(context):
+    # Few notes:
+    #   1. replacing CR/LF with just LF
+    #   2. at the begining, there might also be TTY echo (the command that was sent)
+    context.cmd_stdout = context.shell_session.before.decode().replace("\r\n", "\n")
