73c0c19d00ab76c86f34e141010a1f97f9615093
Pavla Kratochvilova <pkratoch@redhat.com>
2019-01-29 14:53:56 +0100

    [steps] Better check of exact transaction match
    Compare the context table with dnf transaction instead of the rpmdb, since
    some changes (such as group-install, group-remove, or reinstall) are not
    captured in rpmdb.

--- dnf-behave-tests/features/steps/common/behave_ext.py
+++ dnf-behave-tests/features/steps/common/behave_ext.py
@@ -1,6 +1,28 @@
+from .dnf import ACTIONS
+from .rpm import RPM
+from .string import splitter
+
 def check_context_table(context, headings):
     if not context.table:
         raise ValueError("Table not specified.")
 
     if context.table.headings != headings:
         raise ValueError("Invalid table headings. Expected: %s" % ", ".join(headings))
+
+def parse_context_table(context):
+    result = {}
+    for action in ACTIONS.values():
+        result[action] = set()
+
+    for action, nevras in context.table:
+        if action not in result:
+            continue
+        if action.startswith('group-'):
+            for group in splitter(nevras):
+                result[action].add(group)
+        else:
+            for nevra in splitter(nevras):
+                rpm = RPM(nevra)
+                result[action].add(rpm)
+
+    return result
