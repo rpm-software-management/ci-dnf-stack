50ca3d7f7c4f30193f149024eb751122d73eab51
Aleš Matěj <amatej@redhat.com>
2019-02-08 12:50:23 +0100

    [steps] add step matches line by line (block with regex)

--- dnf-behave-tests/features/steps/cmd.py
+++ dnf-behave-tests/features/steps/cmd.py
@@ -181,3 +181,34 @@ def then_stdout_contains_lines(context):
         else:
             print(context.cmd_stderr, file=sys.stderr)
             raise AssertionError("Stderr doesn't contain line: %s" % line)
+
+
+@behave.then("stdout matches line by line")
+def then_stdout_matches_line_by_line(context):
+    out_lines = context.cmd_stdout.split('\n')
+    regexp_lines = context.text.split('\n')
+
+    while out_lines:
+        line = out_lines.pop(0)
+        if line and (not regexp_lines):  # there is no remaining regexp
+            raise AssertionError("Not having a regexp to match line '%s'" % line)
+        elif regexp_lines:
+            regexp = regexp_lines.pop(0).strip()
+            while regexp.startswith('?'):
+                if not re.search(regexp[1:], line):  # optional regexp that doesn't need to be matched
+                    if regexp_lines:
+                        regexp = regexp_lines.pop(0).strip()
+                    else:
+                        raise AssertionError("Not having a regexp to match line '%s'" % line)
+                else:
+                    regexp = regexp[1:]
+            if regexp:
+                if not re.search(regexp, line):
+                    raise AssertionError("'%s' regexp does not match line: '%s'" % (regexp, line))
+
+            else:
+                if not line == "":
+                    raise AssertionError("'%s' is not empty line" % line)
+
+    if regexp_lines:  # there are some unprocessed regexps
+        raise AssertionError("No more line to match regexp '%s'" % regexp_lines[0])
