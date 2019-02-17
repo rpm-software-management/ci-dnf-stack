0c71a09ff958bc1c57f4fcb06619d2fd4e645a97
Daniel Mach <dmach@redhat.com>
2019-02-17 14:28:59 +0100

    New build script that skips unchanged specs.

--- dnf-behave-tests/fixtures/specs/build.sh
+++ dnf-behave-tests/fixtures/specs/build.sh
@@ -13,13 +13,34 @@ GPGDIR="$DIR/../gpgkeys"
 GROUPS_FILENAME="comps.xml"
 UPDATEINFO_FILENAME="updateinfo.xml"
 MODULES_FILENAME="modules.yaml"
-rm -rf "$REPODIR"
 mkdir -p "$REPODIR"
 
 for path in $DIR/*/*.spec; do
     REPO=$(basename $(dirname $path))
-    echo "Building $path..."
-    rpmbuild --quiet --target=$ARCH -ba --nodeps --define "_srcrpmdir $REPODIR/$REPO/src" --define "_rpmdir $REPODIR/$REPO" --define "dist $DIST" $path
+    SPEC_NAME=$(basename $path)
+    SPEC_DIR=$(dirname $path)
+    CSUM_FILE="$SPEC_NAME.sha256"
+    CSUM_CHANGED=0
+
+    # detect spec change -> force rebuild
+    pushd "$SPEC_DIR" > /dev/null
+    if [ -f "$CSUM_FILE" ]; then
+        sha256sum -c --status $CSUM_FILE || CSUM_CHANGED=1
+    else
+        CSUM_CHANGED=1
+    fi
+    popd > /dev/null
+
+    # rebuild changed or new specs
+    if [ $CSUM_CHANGED -eq 1 ]; then
+        echo "Building $path..."
+        rpmbuild --quiet --target=$ARCH -ba --nodeps --define "_srcrpmdir $REPODIR/$REPO/src" --define "_rpmdir $REPODIR/$REPO" --define "dist $DIST" $path
+
+        pushd "$SPEC_DIR" > /dev/null
+        echo "Spec has changed, writing new checksum: $path"
+        sha256sum $(basename $path) > $CSUM_FILE
+        popd > /dev/null
+    fi
 done
 
 ${DIR}/break-packages.sh
@@ -29,7 +50,7 @@ for path in $REPODIR/*; do
     REPO=$(basename $path)
     echo "Creating repo $path..."
     pushd $path
-    ARGS="--no-database"
+    ARGS="--no-database --simple-md-filenames --revision=1550000000 --set-timestamp-to-revision"
     if [ -f ../../specs/$REPO/$GROUPS_FILENAME ]; then
         ARGS="$ARGS --groupfile ../../specs/$REPO/$GROUPS_FILENAME"
     fi
