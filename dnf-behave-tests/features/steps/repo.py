7f38054c2424a0f7d8918385dc5af00d1a4f724a
Michal Domonkos <mdomonko@redhat.com>
2019-02-22 16:21:08 +0100

    [steps] Clean-up func names and imports
    Gets rid of pylint complaints.

--- dnf-behave-tests/features/steps/repo.py
+++ dnf-behave-tests/features/steps/repo.py
@@ -3,26 +3,24 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import behave
-
-from common import *
+from behave import given, step
 
 
-@behave.step("I use the repository \"{repo}\"")
-def step_repo_condition(context, repo):
+@step("I use the repository \"{repo}\"")
+def step_repo_use(context, repo):
     if "repos" not in context.dnf:
         context.dnf["repos"] = []
     if repo not in context.dnf["repos"]:
         context.dnf["repos"].append(repo)
 
 
-@behave.step("I disable the repository \"{repo}\"")
-def step_repo_condition(context, repo):
+@step("I disable the repository \"{repo}\"")
+def step_repo_disable(context, repo):
     if "repos" not in context.dnf:
         context.dnf["repos"] = []
     context.dnf["repos"].remove(repo)
 
 
-@behave.given("There are no repositories")
+@given("There are no repositories")
 def given_no_repos(context):
     context.dnf["reposdir"] = "/dev/null"
