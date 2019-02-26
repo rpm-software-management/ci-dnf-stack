21e7e2f70464679f9b4058a92363463033085b4a
Michal Domonkos <mdomonko@redhat.com>
2019-02-26 11:29:55 +0100

    [features] countme: add first count scenario

--- dnf-behave-tests/features/countme.feature
+++ dnf-behave-tests/features/countme.feature
@@ -21,3 +21,13 @@ Feature: Better counting
          Then every HTTP request to the repository should contain:
             | header     | value                                  |
             | User-Agent | libdnf/X.Y.Z (Fedora 30; Linux.x86_64) |
+
+    @fixture.httpd
+    Scenario: System gets counted for the first time
+        Given my system has not been counted yet
+          And I have enabled a remote repository with metalink support
+          And the repository has the "countme" option set to "1"
+         When I execute dnf with args "countme"
+         Then exactly one HTTP request for the metalink should contain:
+            | URL parameter | value |
+            | countme       | 1     |
--- dnf-behave-tests/features/steps/repo.py
+++ dnf-behave-tests/features/steps/repo.py
@@ -3,6 +3,8 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+import os
+from urllib.parse import urlparse, parse_qs
 from behave import given, when, then, step
 
 from common.rpmdb import get_rpmdb_rpms
@@ -45,9 +47,29 @@ def given_libdnf_version(context):
     context.libdnf_version = rpms[0].version
 
 
+@given("my system has not been counted yet")
+def step_check_counted(context):
+    # We store the countme cookie in the repo cache which should be empty at
+    # this point
+    assert not os.path.exists('%s/var/cache/dnf' % context.dnf.installroot)
+
+
 @given("I have enabled a remote repository")
 def step_remote_repo(context):
     context.dnf["repos"] = ["dnf-ci-http"]
+
+
+@given("I have enabled a remote repository with metalink support")
+def step_remote_repo_metalink(context):
+    context.dnf["repos"] = ["dnf-ci-http-metalink"]
+
+
+@given("the repository has the \"{option}\" option set to \"{value}\"")
+def step_remote_repo_options(context, option, value):
+    repos = context.dnf['repos']
+    assert len(repos) > 0, 'Some repos should be enabled'
+    repo = repos[0]
+    context.dnf['setopts'] = {'%s.%s' % (repo, option): value}
 
 
 @when("I refresh the metadata")
@@ -63,6 +85,10 @@ def step_check_http_requests(context, quantifier, what):
     if what == 'to the repository':
         # We want all requests, hence no filtering
         pass
+    elif what == 'for the metalink':
+        requests = [req for req in requests
+                    if req['path'].startswith('/metalink.xml')]
+
     assert len(requests) > 0, 'Some HTTP requests should have been received'
 
     # Detect what kind of data we have in the table
@@ -82,6 +108,12 @@ def step_check_http_requests(context, quantifier, what):
         matches = [row['value'] == hdr[row['header']]
                    for hdr in headers
                    for row in table]
+    elif 'URL parameter' in headings:
+        paths = [req['path'] for req in requests]
+        queries = [parse_qs(url.query) for url in map(urlparse, paths)]
+        matches = [row['value'] in qry.get(row['URL parameter'], [])
+                   for qry in queries
+                   for row in context.table]
 
     if quantifier == 'every':
         assert all(matches), 'Every request should match the table'
