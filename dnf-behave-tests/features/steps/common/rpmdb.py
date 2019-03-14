12e9223bb14b914135ed4b630379048f2ed6c8cd
Pavla Kratochvilova <pkratoch@redhat.com>
2019-03-14 10:18:50 +0100

    [steps] Check if RPM is gpg-pubkey before arch is parsed.
    gpg-pubkey has no arch, so the decode() method was failing

--- dnf-behave-tests/features/steps/common/rpmdb.py
+++ dnf-behave-tests/features/steps/common/rpmdb.py
@@ -12,11 +12,12 @@ def get_rpmdb_rpms(installroot="/"):
     result = set()
     ts = rpm.TransactionSet(installroot)
     for hdr in ts.dbMatch():
-        decoded = dict(name=hdr["name"].decode(),
+        name = hdr["name"].decode()
+        if name.startswith("gpg-pubkey"):
+            continue
+        decoded = dict(name=name,
                 evr=normalize_epoch(hdr["evr"].decode()),
                 arch=hdr["arch"].decode())
-        if decoded["name"].startswith("gpg-pubkey"):
-            continue
         result.add(RPM("{0[name]}-{0[evr]}.{0[arch]}".format(decoded), hdr))
     return result
 
