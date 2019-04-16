41933b0e9397d6dc561aa1518d191292cb71bf14
Daniel Mach <dmach@redhat.com>
2019-04-16 18:50:28 +0200

    Introduce active tag for matching operating system.
    Use the tags on features or scenarios:
    @<use|not>.with_os=<distro>__<operator>__<version>[.<version>...]
    
    Distro: string representing the operating system
    Operators: "lt", "le", "eq", "ne", "ge", "gt" (same as in Python)
    Version: any dot separated numbers
    
    For example:
    @not.with_os=fedora__eq__28
    @use.with_os=centos__ge__7.1
    
    Then run behave with:
    -D<distro>__<version>
    
    For example:
    -Dos=fedora__30
    -Dos=centos__7

--- dnf-behave-tests/features/environment.py
+++ dnf-behave-tests/features/environment.py
@@ -3,10 +3,13 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-from behave import fixture, use_fixture
+import operator
 import os
 import shutil
 import tempfile
+
+from behave import fixture, use_fixture
+from behave.tag_matcher import ActiveTagMatcher
 
 from steps.fixtures.httpd import HttpServerContext
 
@@ -19,6 +22,55 @@ DEFAULT_REPOSDIR = os.path.join(FIXTURES_DIR, "repos.d")
 DEFAULT_REPOS_LOCATION = os.path.join(FIXTURES_DIR, "repos")
 DEFAULT_RELEASEVER="29"
 DEFAULT_PLATFORM_ID="platform:f29"
+
+
+class VersionedActiveTagMatcher(ActiveTagMatcher):
+    @staticmethod
+    def version_compare_operator(tag_value_str, current_value_str, is_negated=False):
+        tag_key, tag_oper, tag_value = tag_value_str.split("__", 2)
+        current_key, current_value = current_value_str.split("__", 1)
+
+        # convert versions into list of integers for correct comparing
+        tag_value = [int(i) for i in tag_value.split(".")]
+        current_value = [int(i) for i in current_value.split(".")]
+
+        if tag_oper not in ["lt", "le", "eq", "ne", "ge", "gt"]:
+            raise ValueError("Invalid operator in tag: %s" % tag_value_str)
+
+        if tag_key != current_key:
+            # always return false if keys do not match
+            return False
+
+        op = getattr(operator, tag_oper)
+        result = op(current_value, tag_value)
+        if is_negated:
+            result = not(result)
+        return result
+
+    def is_tag_group_enabled(self, group_category, group_tag_pairs):
+        """Modified ActiveTagMatcher.is_tag_group_enabled()"""
+        if not group_tag_pairs:
+            # -- CASE: Empty group is always enabled (CORNER-CASE).
+            return True
+
+        current_value = self.value_provider.get(group_category, None)
+        if current_value is None and self.ignore_unknown_categories:
+            # -- CASE: Unknown category, ignore it.
+            return True
+
+        tags_enabled = []
+        for category_tag, tag_match in group_tag_pairs:
+            tag_prefix = tag_match.group("prefix")
+            category = tag_match.group("category")
+            tag_value = tag_match.group("value")
+            assert category == group_category
+            tag_enabled = self.version_compare_operator(tag_value, current_value, self.is_tag_negated(tag_prefix))
+            tags_enabled.append(tag_enabled)
+        return any(tags_enabled)    # -- PROVIDES: LOGICAL-OR expression
+
+
+active_tag_value_provider = {}
+active_tag_matcher = VersionedActiveTagMatcher(active_tag_value_provider)
 
 
 class DNFContext(object):
@@ -141,6 +193,8 @@ def after_step(context, step):
 
 
 def before_scenario(context, scenario):
+    if active_tag_matcher.should_exclude_with(scenario.effective_tags):
+        scenario.skip(reason="DISABLED ACTIVE-TAG")
     if not context.feature_global_dnf_context:
         context.dnf = DNFContext(context.config.userdata,
                                  tmp_installroot='force_tmp_installroot' in scenario.tags)
@@ -172,7 +226,9 @@ def after_tag(context, tag):
 
 
 def before_all(context):
-    pass
+    os = context.config.userdata.get("os", None)
+    if os:
+        active_tag_value_provider["os"] = os
 
 
 def after_all(context):
