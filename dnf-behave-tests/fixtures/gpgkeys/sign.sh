fbc73b4bc061155600d921a32534049280d2bfe6
Marek Blaha <mblaha@redhat.com>
2019-05-16 12:13:28 +0200

    [test data] Use absolute path for HOME when signing packages

--- dnf-behave-tests/fixtures/gpgkeys/sign.sh
+++ dnf-behave-tests/fixtures/gpgkeys/sign.sh
@@ -8,12 +8,12 @@ SPECS="$DIR/../specs"
 pushd ${DIR}
 for KEY_NAME in $(ls -d */ | sed 's#/##'); do
     # import public and private key
-    HOME=${KEY_NAME} gpg2 --import "${KEY_NAME}/${KEY_NAME}-public"
-    HOME=${KEY_NAME} gpg2 --import "${KEY_NAME}/${KEY_NAME}-private"
+    HOME=$(readlink -e ${KEY_NAME}) gpg2 --import "${KEY_NAME}/${KEY_NAME}-public"
+    HOME=$(readlink -e ${KEY_NAME}) gpg2 --import "${KEY_NAME}/${KEY_NAME}-private"
 
     # sign packages
     for package in $(cat "${KEY_NAME}/signed-packages"); do
-        HOME=${KEY_NAME} rpm --addsign "${REPODIR}/${package}"
+        HOME=$(readlink -e ${KEY_NAME}) rpm --addsign "${REPODIR}/${package}"
     done
 done
-
+popd
