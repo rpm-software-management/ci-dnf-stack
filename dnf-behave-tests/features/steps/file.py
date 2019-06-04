e1d046a2f9b9003a0473ff09596f4687ca79e8bc
Marek Blaha <mblaha@redhat.com>
2019-06-04 09:43:36 +0200

    [steps] New syntax for paths that ar not inside installroot
    Paths that begin with two backslashes are considered to be absolute
    paths not inside installroot.
    
    /the/directory -> /install/root/the/directory
    //the/directory -> /the/directory

--- dnf-behave-tests/features/steps/file.py
+++ dnf-behave-tests/features/steps/file.py
@@ -9,54 +9,61 @@ import os
 
 from common import *
 
+def prepend_installroot(context, path):
+    path = path.format(context=context)
+    root = '/'
+    if not path.startswith('//'):
+        root = context.dnf.installroot
+    return os.path.join(root, path.lstrip("/"))
+
 @behave.given('I create directory "{dirpath}"')
 def step_impl(context, dirpath):
-    full_path = os.path.join(context.dnf.installroot, dirpath.lstrip("/"))
+    full_path = prepend_installroot(context, dirpath)
     ensure_directory_exists(full_path)
 
 
 @behave.given('I create file "{filepath}" with')
 def step_impl(context, filepath):
-    full_path = os.path.join(context.dnf.installroot, filepath.lstrip("/"))
+    full_path = prepend_installroot(context, filepath)
     ensure_directory_exists(os.path.dirname(full_path))
     create_file_with_contents(full_path, context.text)
 
 
 @behave.given('I create and substitute file "{filepath}" with')
 def step_impl(context, filepath):
-    full_path = os.path.join(context.dnf.installroot, filepath.lstrip("/"))
+    full_path = prepend_installroot(context, filepath)
     ensure_directory_exists(os.path.dirname(full_path))
     create_file_with_contents(full_path, context.text.format(context=context))
 
 
 @behave.given('I delete file "{filepath}"')
 def step_delete_file(context, filepath):
-    full_path = os.path.join(context.dnf.installroot, filepath.lstrip("/"))
+    full_path = prepend_installroot(context, filepath)
     delete_file(full_path)
 
 
 @behave.given('I delete directory "{dirpath}"')
 def step_delete_directory(context, dirpath):
-    full_path = os.path.join(context.dnf.installroot, dirpath.lstrip("/"))
+    full_path = prepend_installroot(context, dirpath)
     delete_directory(full_path)
 
 
 @behave.step('file "{filepath}" exists')
 def file_exists(context, filepath):
-    full_path = os.path.join(context.dnf.installroot, filepath.lstrip("/"))
+    full_path = prepend_installroot(context, filepath)
     ensure_file_exists(full_path)
 
 
 @behave.given('file "{filepath}" does not exist')
 def file_does_not_exist(context, filepath):
-    full_path = os.path.join(context.dnf.installroot, filepath.lstrip("/"))
+    full_path = prepend_installroot(context, filepath)
     ensure_file_does_not_exist(full_path)
 
 
 @behave.step('file "{filepath}" contains lines')
 def file_contains(context, filepath):
     regexp_lines = context.text.split('\n')
-    full_path = os.path.join(context.dnf.installroot, filepath.lstrip("/"))
+    full_path = prepend_installroot(context, filepath)
     ensure_directory_exists(os.path.dirname(full_path))
     read_str = read_file_contents(full_path)
     for line in regexp_lines:
@@ -69,7 +76,7 @@ def file_contains(context, filepath):
 @behave.step('I copy directory "{source}" to "{destination}"')
 def step_impl(context, source, destination):
     source = source.format(context=context)
-    destination = os.path.join(context.dnf.installroot, destination.lstrip("/"))
+    destination = prepend_installroot(context, destination)
     ensure_directory_exists(os.path.dirname(destination))
     copy_tree(source, destination)
 
