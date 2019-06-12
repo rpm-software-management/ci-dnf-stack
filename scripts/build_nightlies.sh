a2f286366f1e652a01c9ecf2739b9ea5f217200e
Marek Blaha <mblaha@redhat.com>
2019-06-12 10:01:01 +0200

    Run builds for each target in parallel

--- scripts/build_nightlies.sh
+++ scripts/build_nightlies.sh
@@ -4,19 +4,28 @@ set -xeuo pipefail
 OWNER="rpmsoftwaremanagement"
 PROJECT="dnf-nightly"
 CHROOTS=(
-          "epel-7-x86_64"
-          "fedora-28-x86_64"
-          "fedora-29-x86_64"
           "fedora-30-x86_64"
+          "fedora-29-x86_64"
           "fedora-rawhide-x86_64"
+          #"epel-7-x86_64"
           )
+
+run_build(){
+    overlaydir="dnf-master"
+    if [[ $chroot == epel-* ]] ; then
+        overlaydir="$overlaydir-epel7"
+    fi
+    gitdir=$(mktemp -d tmp_nightlies.XXXXXXXX --tmpdir)
+    rpm-gitoverlay --gitdir "$gitdir" --log DEBUG build-overlay -s ../overlays/$overlaydir rpm copr --chroot "$1" --owner $OWNER --project $PROJECT
+    rm -rf "$gitdir"
+}
 
 pushd $(dirname $(readlink -f $0))
     for chroot in "${CHROOTS[@]}"; do
-        overlaydir="dnf-master"
-        if [[ $chroot == epel-* ]] ; then
-            overlaydir="$overlaydir-epel7"
-        fi
-        rpm-gitoverlay --log DEBUG build-overlay -s ../overlays/$overlaydir rpm copr --chroot "$chroot" --owner $OWNER --project $PROJECT
+        run_build "$chroot" &
     done
 popd
+
+wait
+
+echo "Done."
