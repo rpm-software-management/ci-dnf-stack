2ef945830e4dc8cd80fe85bbfba7fe4dda9ea007
Michal Domonkos <mdomonko@redhat.com>
2019-07-26 14:28:25 +0200

    [steps] Fix get_rpmdb_rpms() for F31
    Latest RPM versions do return utf-8 strings so we no longer need to
    decode there (but we still do on F30).
    
    More details:
    https://github.com/rpm-software-management/rpm/commit/84920f898315d09a57a3f1067433eaeb7de5e830

--- dnf-behave-tests/features/steps/common/rpmdb.py
+++ dnf-behave-tests/features/steps/common/rpmdb.py
@@ -5,9 +5,14 @@ from __future__ import print_function
 
 import rpm
 
-from .cmd import run
 from .rpm import normalize_epoch
 from .rpm import RPM
+
+
+def _str(obj):
+    if isinstance(obj, bytes):
+        return obj.decode()
+    return obj
 
 
 def get_rpmdb_rpms(installroot="/"):
@@ -17,12 +22,11 @@ def get_rpmdb_rpms(installroot="/"):
     result = set()
     ts = rpm.TransactionSet(installroot)
     for hdr in ts.dbMatch():
-        name = hdr["name"].decode()
+        name = _str(hdr["name"])
+        evr = _str(hdr["evr"])
+        arch = _str(hdr["arch"])
         if name.startswith("gpg-pubkey"):
             continue
-        decoded = dict(name=name,
-                evr=normalize_epoch(hdr["evr"].decode()),
-                arch=hdr["arch"].decode())
-        result.add(RPM("{0[name]}-{0[evr]}.{0[arch]}".format(decoded), hdr))
+        evr = normalize_epoch(evr)
+        result.add(RPM("%s-%s.%s" % (name, evr, arch), hdr))
     return result
-
