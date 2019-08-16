928fcf85e5efdecadb575e4d152e5f39269ef56a
Daniel Mach <dmach@redhat.com>
2019-08-16 15:57:15 +0200

    Fix crashing get_rpmdb_rpms() on Fedora 31.

--- dnf-behave-tests/features/steps/common/rpmdb.py
+++ dnf-behave-tests/features/steps/common/rpmdb.py
@@ -10,6 +10,13 @@ from .rpm import normalize_epoch
 from .rpm import RPM
 
 
+def _get_hdr_str(hdr, name):
+    try:
+        return hdr[name].decode()
+    except AttributeError:
+        return hdr[name]
+
+
 def get_rpmdb_rpms(installroot="/"):
     """
     Read all installed RPMs from RPM database.
@@ -17,12 +24,14 @@ def get_rpmdb_rpms(installroot="/"):
     result = set()
     ts = rpm.TransactionSet(installroot)
     for hdr in ts.dbMatch():
-        name = hdr["name"].decode()
+        name = _get_hdr_str(hdr, "name")
         if name.startswith("gpg-pubkey"):
             continue
-        decoded = dict(name=name,
-                evr=normalize_epoch(hdr["evr"].decode()),
-                arch=hdr["arch"].decode())
+        decoded = {
+            "name": name,
+            "evr": normalize_epoch(_get_hdr_str(hdr, "evr")),
+            "arch": _get_hdr_str(hdr, "arch"),
+        }
         result.add(RPM("{0[name]}-{0[evr]}.{0[arch]}".format(decoded), hdr))
     return result
 
