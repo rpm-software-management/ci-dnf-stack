1e6ab8da7e1ebe0753c9393d58d67e04782f4170
ksrot <ksrot@redhat.com>
2019-08-26 13:03:33 +0200

    implement step "stderr matches line by line"

--- dnf-behave-tests/features/steps/cmd.py
+++ dnf-behave-tests/features/steps/cmd.py
@@ -282,28 +282,11 @@ def then_stdout_contains_lines(context):
 def then_stdout_matches_line_by_line(context):
     out_lines = context.cmd_stdout.split('\n')
     regexp_lines = context.text.split('\n')
+    lines_match_to_regexps_line_by_line(out_lines, regexp_lines)
 
-    while out_lines:
-        line = out_lines.pop(0)
-        if line and (not regexp_lines):  # there is no remaining regexp
-            raise AssertionError("Not having a regexp to match line '%s'" % line)
-        elif regexp_lines:
-            regexp = regexp_lines.pop(0).strip()
-            while regexp.startswith('?'):
-                if not re.search(regexp[1:], line):  # optional regexp that doesn't need to be matched
-                    if regexp_lines:
-                        regexp = regexp_lines.pop(0).strip()
-                    else:
-                        raise AssertionError("Not having a regexp to match line '%s'" % line)
-                else:
-                    regexp = regexp[1:]
-            if regexp:
-                if not re.search(regexp, line):
-                    raise AssertionError("'%s' regexp does not match line: '%s'" % (regexp, line))
 
-            else:
-                if not line == "":
-                    raise AssertionError("'%s' is not empty line" % line)
-
-    if regexp_lines:  # there are some unprocessed regexps
-        raise AssertionError("No more line to match regexp '%s'" % regexp_lines[0])
+@behave.then("stderr matches line by line")
+def then_stderr_matches_line_by_line(context):
+    out_lines = context.cmd_stderr.split('\n')
+    regexp_lines = context.text.split('\n')
+    lines_match_to_regexps_line_by_line(out_lines, regexp_lines)
