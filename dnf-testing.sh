debe2c97d5ef6c68e38ffb9727bfe7b4a8f672db
Marek Blaha <mblaha@redhat.com>
2020-05-18 15:30:33 +0200

    Prefer platform-python if present

--- dnf-testing.sh
+++ dnf-testing.sh
@@ -4,9 +4,15 @@ PROG_PATH=$(dirname $(realpath -s -- $0))
 
 pushd $PROG_PATH > /dev/null
 
-PYTHON=`command -v python3`
-if [ -z $PYTHON ]; then
-    PYTHON=`command -v python2`
+# first check whether platform-python binary exists (for RHEL8)
+if [[ -x "/usr/libexec/platform-python" ]]; then
+    PYTHON=/usr/libexec/platform-python
+else
+    # use python3 with fall back to python2
+    PYTHON=`command -v python3`
+    if [ -z $PYTHON ]; then
+        PYTHON=`command -v python2`
+    fi
 fi
 if [ ! $PYTHON ]; then
     printf >&2 "Error: Python interpreter not found.\n"
