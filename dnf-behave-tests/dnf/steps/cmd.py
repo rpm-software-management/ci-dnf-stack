8a3bd9eef428e7c2303b7a8879e0351c62224530
Evan Goode <mail@evangoo.de>
2023-02-03 17:20:52 -0500

    Add tests for transaction.test()
    Requires https://github.com/rpm-software-management/dnf5/pull/251
    
    Signed-off-by: Evan Goode <mail@evangoo.de>

--- dnf-behave-tests/dnf/steps/cmd.py
+++ dnf-behave-tests/dnf/steps/cmd.py
@@ -458,6 +458,17 @@ def execute_transaction(goal,description):
     transaction.set_description(description)
     transaction.run()
 
+def test_transaction(goal):
+    transaction = goal.resolve()
+    for tspkg in transaction.get_transaction_packages():
+        print(tspkg.get_package().get_nevra(), ":", libdnf5.base.transaction.transaction_item_action_to_string(tspkg.get_action()))
+    downloader = libdnf5.repo.PackageDownloader()
+    for tspkg in transaction.get_transaction_packages():
+        if libdnf5.base.transaction.transaction_item_action_is_inbound(tspkg.get_action()):
+            downloader.add(tspkg.get_package())
+    downloader.download(True, True)
+    return transaction.test()
+
 base = libdnf5.base.Base()
 config = base.get_config()
 vars = base.get_vars()
