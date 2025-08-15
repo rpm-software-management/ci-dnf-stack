cbafcf349af36ea36677a54bcfc2c4aea3ab0abf
Eva Mrakova <emrakova@redhat.com>
2025-08-15 14:49:42 +0200

    Use a mixture of classic and PQC certificates

--- dnf-behave-tests/fixtures/certificates/generate_certificates.sh
+++ dnf-behave-tests/fixtures/certificates/generate_certificates.sh
@@ -7,12 +7,23 @@ PROG_PATH="$(dirname "$(readlink -f -- "$0")")"
 rm -rf "$PROG_PATH/testcerts"
 mkdir -p "$PROG_PATH/testcerts"
 pushd "$PROG_PATH/testcerts"
-x509KeyGen ca
-x509KeyGen server
-x509KeyGen client
-x509KeyGen ca2
-x509KeyGen server2
-x509KeyGen client2
+[ -n "$CIPH1" ] || CIPH1="rsa"
+if [ -z "$CIPH2" ] ; then
+    # ensure that PQC is supported
+    openssl list -signature-algorithms &> openssl.log
+    if grep -i mldsa65 openssl.log; then
+        CIPH2="mldsa65"
+    else
+        CIPH2="rsa"
+    fi
+    rm -f openssl.log
+fi
+x509KeyGen -t "$CIPH1" ca
+x509KeyGen -t "$CIPH1" server
+x509KeyGen -t "$CIPH1" client
+x509KeyGen -t "$CIPH2" ca2
+x509KeyGen -t "$CIPH2" server2
+x509KeyGen -t "$CIPH2" client2
 x509SelfSign -t ca ca
 x509SelfSign -t ca ca2
 x509CertSign -t webserver --CA ca server
