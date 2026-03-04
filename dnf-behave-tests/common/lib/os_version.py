23430f8a8b0004801483489a900bf4f0ad4a69df
Petr Písař <ppisar@redhat.com>
2026-03-04 14:10:11 +0100

    Handle Fedora ELN as RHEL
    We compile the tested software according to %rhel RPM macro. To skip
    or condition tests correctly, we need to handle Fedora ELN as RHEL
    when evaluating behave tags.
    
    Old dection had these problems: distro.id() parses /etc/os-release.
    That file declares ELN in VARIANT constant, but distro.id() does not
    recognizes that (correctly because ELN is still a Fedora project).
    Also /etc/os-release does not contain the RHEL version.
    
    This patch overrides the distr.id() detection with querying RPM
    library for %rhel macro. The same what we use when building the
    software.

--- dnf-behave-tests/common/lib/os_version.py
+++ dnf-behave-tests/common/lib/os_version.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import distro
+import rpm
 
 
 def detect_os_version():
     os_id = distro.id()
+    major_version = distro.major_version()
 
     # treat centos as RHEL in context of scenario tag matching
     if os_id == "centos":
         os_id = "rhel"
 
-    return os_id + "__" + distro.major_version()
+    # Treat anything that defines non-empty "rhel" RPM macro as RHEL.
+    # That's especially needed to handle Fedora ELN as RHEL.
+    rhel_macro_version = int(rpm.expandMacro("0%{?rhel}"))
+    if rhel_macro_version > 0:
+        os_id = "rhel"
+        major_version = str(rhel_macro_version)
+
+    return os_id + "__" + major_version
